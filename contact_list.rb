d2ff88d2ceb79d46ab2da919f79cc598ee515a4e
goodbru <ryangoodhew@gmail.com>
2016-04-05 02:41:43 +0000

    command 1 commit

--- contact_list.rb
+++ contact_list.rb
@@ -1,8 +1,30 @@
-require_relative 'contact'
+# require_relative 'contact'
 
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
 
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+	# Step 1: When the app is run with no command, it should display a menu like this.
+	
 
+
+	if  ARGV.empty?
+		puts "Here is a list of available commands: "
+		puts "new	 - Create a new contact "
+		puts "list 	 - List all contacts "
+		puts "show 	 - Show a contact "
+		puts "search - Search contacts "
+	end
+		# ARGV.each do |a| 
+		# 	puts "#{a}"
+		# end
+	# def show_menu 
+
+	# 	puts "Here is a list of available commands: "
+	# 	puts "new	 - Create a new contact "
+	# 	puts "list 	 - List all contacts "
+	# 	puts "show 	 - Show a contact "
+	# 	puts "search - Search contacts "
+	# end
+	# show_menu
 end
