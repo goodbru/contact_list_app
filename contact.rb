73b5d6799f13b6c348373dd0a5e44a63e8793cfa
goodbru <ryangoodhew@gmail.com>
2016-04-06 23:31:45 +0000

    check to see if yo mamma is in my contacts list

--- contact.rb
+++ contact.rb
@@ -4,45 +4,70 @@ require 'csv'
 # The ContactList class will work with Contact objects instead of interacting with the CSV file directly
 class Contact
 
-  attr_accessor :name, :email
-  
+  attr_accessor :name, :email, :id
+
   # Creates a new contact object
   # @param name [String] The contact's name
   # @param email [String] The contact's email address
-  def initialize(name, email)
+  def initialize(id, name, email)
     # TODO: Assign parameter values to instance variables.
+    @name = name
+    @email = email
+    @id = id
   end
 
   # Provides functionality for managing contacts in the csv file.
   class << self
 
+
     # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
     # @return [Array<Contact>] Array of Contact objects
     def all
       # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
+      all_array = []
+      CSV.foreach('contacts.csv') do |id,name,email|
+        new_contact = Contact.new(id,name,email)
+        all_array << new_contact
+      end
+      return all_array
     end
+
 
     # Creates a new contact, adding it to the csv file, returning the new contact.
     # @param name [String] the new contact's name
     # @param email [String] the contact's email
-    def create(name, email)
+    def create(id, name, email)
       # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
+      CSV.open('contacts.csv', 'a+') do |csv|
+        new_contact = Contact.new(id,name,email)
+        csv << [id, name, email]
+      end
+
     end
-    
+
     # Find the Contact in the 'contacts.csv' file with the matching id.
     # @param id [Integer] the contact id
     # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
       # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
+      CSV.foreach('contacts.csv') do |row|
+        if id == row[0]
+          return row
+        end
+      end
+         return nil
     end
-    
-    # Search for contacts by either name or email.
-    # @param term [String] the name fragment or email fragment to search for
-    # @return [Array<Contact>] Array of Contact objects.
-    def search(term)
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+        # Search for contacts by either name or email.
+        # @param term [String] the name fragment or email fragment to search for
+        # @return [Array<Contact>] Array of Contact objects.
+        def search(word)
+          # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+          CSV.foreach('contacts.csv') do |row|
+            row.each do |elm|
+              if elm =~ /#{word}/i
+              end
+            end
+          end
+        end
+      end
     end
-
-  end
-
-end
--- contact_list.rb
+++ contact_list.rb
@@ -1,30 +1,79 @@
-# require_relative 'contact'
-
+require_relative 'contact'
+require 'csv'
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
 
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
-	# Step 1: When the app is run with no command, it should display a menu like this.
-	
+  # Step 1: When the app is run with no command, it should display a menu like this.
 
 
-	if  ARGV.empty?
-		puts "Here is a list of available commands: "
-		puts "new	 - Create a new contact "
-		puts "list 	 - List all contacts "
-		puts "show 	 - Show a contact "
-		puts "search - Search contacts "
-	end
-		# ARGV.each do |a| 
-		# 	puts "#{a}"
-		# end
-	# def show_menu 
 
-	# 	puts "Here is a list of available commands: "
-	# 	puts "new	 - Create a new contact "
-	# 	puts "list 	 - List all contacts "
-	# 	puts "show 	 - Show a contact "
-	# 	puts "search - Search contacts "
-	# end
-	# show_menu
+  def self.contact_index
+    contact_array = Contact.all
+    contact_array.each do |contact|
+      puts contact.id + " " + contact.name + " " + contact.email
+    end
+    puts '---'
+    puts "#{contact_array.length} records total"
+  end
+
+
+  def self.new_contact
+    print "What is the new contacts first and last name: "
+    contact_name = STDIN.gets.chomp
+    print "What is the email address: "
+    contact_email = STDIN.gets.chomp
+    new_row_index = CSV.read('contacts.csv').length + 1
+    new_contact = Contact.create(new_row_index,contact_name,contact_email)
+  end
+
+
+  def self.show_contact(id)
+    row = Contact.find(id)
+    
+    if row
+      puts row[1] + " " + row[2]
+    else
+      puts "Not found"
+    end
+  end
+
+
+
+  def self.search_contact(word)
+    word_match = 0
+    record_match = false
+    search_contact = Contact.search(word)
+
+    if record_match == true
+      puts row[0] + " " + row[1] + " " + row[2]
+      word_match += 1
+    end #end-if
+
+    puts '---'
+    puts "#{word_match} records total"
+  end #end-def
+
+end
+
+
+
+if  ARGV.empty?
+  puts "Here is a list of available commands: "
+  puts "new    - Create a new contact "
+  puts "list   - List all contacts "
+  puts "show   - Show a contact "
+  puts "search - Search contacts "
+
+else
+  case ARGV[0]
+  when 'list'
+    ContactList.contact_index
+  when 'new'
+    ContactList.new_contact
+  when 'show'
+    ContactList.show_contact(ARGV[1])
+  when 'search'
+    ContactList.search_contact(ARGV[1])
+  end
 end
