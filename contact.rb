544f05e28b9243e6e6211ea13c698acff8d1e9b2
goodbru <ryangoodhew@gmail.com>
2016-04-14 02:39:49 +0000

    active recordized

--- contact.rb
+++ contact.rb
@@ -1,102 +1,102 @@
-require 'pg'
-require 'csv'
+# require 'pg'
+# require 'csv'
 # require_relative 'contact_list'
 
 # Represents a person in an address book.
 # The ContactList class will work with Contact objects instead of interacting with the CSV file directly
-class Contact
+class Contact < ActiveRecord::Base
 
-  # Code to setup the database. Should only be run once.
-  # Assumes you have a database called namedb.
-
-
-  attr_accessor :name, :email, :id, :conn
-
-  # Creates a new contact object
-  # @param name [String] The contact's name
-  # @param email [String] The contact's email address
-  def initialize(id, name, email)
-    # TODO: Assign parameter values to instance variables.
-    @name = name
-    @email = email
-    @id = id
-    @all_array = []
-  end
+  # # Code to setup the database. Should only be run once.
+  # # Assumes you have a database called namedb.
 
 
-  def self.set_connection(conn)
-    @@conn = conn
-  end
+  # attr_accessor :name, :email, :id, :conn
 
-  def destroy
-    @@conn.exec_params("DELETE FROM contacts WHERE id = $1::int;", [@id])
-  end
-
-  def save
-    @@conn.exec_params("UPDATE contacts SET name = $2, email = $3 WHERE id = $1::int", [@id.to_s, @name, @email])
-  end
-
-
-  # Provides functionality for managing contacts in the csv file.
-  class << self
-
-    def save(attributes)
-      @@conn.exec_params("INSERT INTO contacts (name, email) VALUES ($1, $2)", [attributes[:name], attributes[:email]])
-      #@@conn.exec_params("UPDATE contacts SET name = $1, email = $2 WHERE id = $1::int", [attributes[:id], attributes[:name], attributes[:email]])
-    end
+  # # Creates a new contact object
+  # # @param name [String] The contact's name
+  # # @param email [String] The contact's email address
+  # def initialize(id, name, email)
+  #   # TODO: Assign parameter values to instance variables.
+  #   @name = name
+  #   @email = email
+  #   @id = id
+  #   @all_array = []
+  # end
 
 
-    # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
-    # @return [Array<Contact>] Array of Contact objects
-    def all
-      # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
+  # def self.set_connection(conn)
+  #   @@conn = conn
+  # end
 
-      result = @@conn.exec("SELECT * FROM contacts")
-      # CSV.foreach('contacts.csv') do |id,name,email|
-      all_array = []
-      result.each_row do |row|
-        id = row[0]
-        name = row[1]
-        email = row[2]
-        all_array << Contact.new(id,name,email)
-      end
-      return all_array
-    end
+  # def destroy
+  #   @@conn.exec_params("DELETE FROM contacts WHERE id = $1::int;", [@id])
+  # end
+
+  # def save
+  #   @@conn.exec_params("UPDATE contacts SET name = $2, email = $3 WHERE id = $1::int", [@id.to_s, @name, @email])
+  # end
 
 
-    # Creates a new contact, adding it to the csv file, returning the new contact.
-    # @param name [String] the new contact's name
-    # @param email [String] the contact's email
-    def create(attributes)
-      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
-      #CSV.open('contacts.csv', 'a+') do |csv|
-      self.save(attributes)
-    end
+  # # Provides functionality for managing contacts in the csv file.
+  # class << self
 
-    # Find the Contact in the 'contacts.csv' file with the matching id.
-    # @param id [Integer] the contact id
-    # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
-    def find(id)
-      # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
-      result = @@conn.exec("SELECT * FROM contacts WHERE id = $1::int", [id])
-       return result.map { |row| Contact.new row["id"], row["name"], row["email"] }.first    
-      # result.each_row do |row|
-      #   if id == row[0]
-      #     return row
-      #   end
-      # end
+  #   def save(attributes)
+  #     @@conn.exec_params("INSERT INTO contacts (name, email) VALUES ($1, $2)", [attributes[:name], attributes[:email]])
+  #     #@@conn.exec_params("UPDATE contacts SET name = $1, email = $2 WHERE id = $1::int", [attributes[:id], attributes[:name], attributes[:email]])
+  #   end
 
-    end
-    # Search for contacts by either name or email.
-    # @param term [String] the name fragment or email fragment to search for
-    # @return [Array<Contact>] Array of Contact objects.
-    def search(word)
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
-      # CSV.foreach('contacts.csv') do |row|
-      #   row.each do |elm|
-      #     if elm =~ /#{word}/i
-      result = @@conn.exec("SELECT * FROM contacts WHERE name ilike '%#{word}%'")   #'/%#{word}%/i'
-      return result.map { |row| Contact.new row["id"], row["name"], row["email"] }
-    end
-  end
+
+  #   # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
+  #   # @return [Array<Contact>] Array of Contact objects
+  #   def all
+  #     # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
+
+  #     result = @@conn.exec("SELECT * FROM contacts")
+  #     # CSV.foreach('contacts.csv') do |id,name,email|
+  #     all_array = []
+  #     result.each_row do |row|
+  #       id = row[0]
+  #       name = row[1]
+  #       email = row[2]
+  #       all_array << Contact.new(id,name,email)
+  #     end
+  #     return all_array
+  #   end
+
+
+  #   # Creates a new contact, adding it to the csv file, returning the new contact.
+  #   # @param name [String] the new contact's name
+  #   # @param email [String] the contact's email
+  #   def create(attributes)
+  #     # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
+  #     #CSV.open('contacts.csv', 'a+') do |csv|
+  #     self.save(attributes)
+  #   end
+
+  #   # Find the Contact in the 'contacts.csv' file with the matching id.
+  #   # @param id [Integer] the contact id
+  #   # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
+  #   def find(id)
+  #     # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
+  #     result = @@conn.exec("SELECT * FROM contacts WHERE id = $1::int", [id])
+  #      return result.map { |row| Contact.new row["id"], row["name"], row["email"] }.first
+  #     # result.each_row do |row|
+  #     #   if id == row[0]
+  #     #     return row
+  #     #   end
+  #     # end
+
+  #   end
+  #   # Search for contacts by either name or email.
+  #   # @param term [String] the name fragment or email fragment to search for
+  #   # @return [Array<Contact>] Array of Contact objects.
+  #   def search(word)
+  #     # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+  #     # CSV.foreach('contacts.csv') do |row|
+  #     #   row.each do |elm|
+  #     #     if elm =~ /#{word}/i
+  #     result = @@conn.exec("SELECT * FROM contacts WHERE name ilike '%#{word}%'")   #'/%#{word}%/i'
+  #     return result.map { |row| Contact.new row["id"], row["name"], row["email"] }
+  #   end
+  # end
 end
--- contact_list.rb
+++ contact_list.rb
@@ -1,17 +1,28 @@
-require 'csv'
+require 'active_record'
 require 'pg'
 require_relative './contact'
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
+  puts "Connecting to the database..."
+
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
   # Step 1: When the app is run with no command, it should display a menu like this.
-  conn = PG::Connection.new(dbname: 'contactdb', host: 'localhost', user: 'vagrant', password: 'password')
-  Contact.set_connection(conn)
+  # conn = PG::Connection.new(dbname: 'contactdb', host: 'localhost', user: 'vagrant', password: 'password')
+  # Contact.set_connection(conn)
+
+  ActiveRecord::Base.establish_connection(
+    adapter: "postgresql",
+    host: "localhost",
+    username: "vagrant",
+    password: "password",
+  database: "contactdb"  )
+
+  puts "Welcome to the Contact List"
 
   def self.contact_index
     contact_array = Contact.all
     contact_array.each do |contact|
-      puts contact.id + " " + contact.name + " " + contact.email
+      puts "#{contact.id} #{contact.name} #{contact.email}"
     end
     puts '---'
     puts "#{contact_array.length} records total"
@@ -28,43 +39,45 @@ class ContactList
   end
 
   def self.update_contact(id)
-    contact = Contact.find(id)
+    contact = Contact.find_by(id: ARGV[1])
+    # contact = Contact.find(id)
     print "What is the contacts new first and last name: "
     new_name = STDIN.gets.chomp
     contact.name = new_name
     print "What is the new email address: "
     new_email = STDIN.gets.chomp
     contact.email = new_email
-    contact.save 
-    puts "#{contact.id} #{contact.name} #{contact.email}"
+    contact.save
+    # puts "#{contact.id} #{contact.name} #{contact.email}"
   end
 
   def self.show_contact(id)
-      contact = Contact.find(id)
-      puts "#{contact.id} #{contact.name} #{contact.email}"      # show_array = []
-      # result.each_row do |row|
-      #     id = row[0]
-      #     name = row[1]
-      #     email = row[2]
-      #     show_array << Contact.search(id)
-      # end
-      # return show_array
-      # if row
-      #   puts row[1] + " " + row[2]
-      # else
-      #   puts "Not found"
-      # end
+    contact = Contact.find_by(id: ARGV[1])
+    puts "#{contact.id} #{contact.name} #{contact.email}"      # show_array = []
+    # result.each_row do |row|
+    #     id = row[0]
+    #     name = row[1]
+    #     email = row[2]
+    #     show_array << Contact.search(id)
+    # end
+    # return show_array
+    # if row
+    #   puts row[1] + " " + row[2]
+    # else
+    #   puts "Not found"
+    # end
   end
 
   def self.destroy_contact(id)
     contact = Contact.find(id)
     contact.destroy
+    contact.save
   end
 
   def self.search_contact(word)
     # word_match = 0
     # record_match = false
-    contacts = Contact.search(word)
+    contacts = Contact.where("name ILIKE ? OR email ILIKE ?", "%#{word}%", "%#{word}%")
 
     # if record_match == true
     #   puts row[0] + " " + row[1] + " " + row[2]
@@ -75,29 +88,29 @@ class ContactList
     }
     # # puts '---'
     # puts "#{word_match} records total"
-    
-  end #end-def
-if  ARGV.empty?
-  puts "Here is a list of available commands: "
-  puts "new    - Create a new contact "
-  puts "list   - List all contacts "
-  puts "show   - Show a contact "
-  puts "search - Search contacts "
 
-else
-  case ARGV[0]
-  when 'list'
-    ContactList.contact_index
-  when 'new'
-    ContactList.new_contact
-  when 'show'
-    ContactList.show_contact(ARGV[1])
-  when 'search'
-    ContactList.search_contact(ARGV[1])
-  when 'update'
-    ContactList.update_contact(ARGV[1])
-  when 'destroy'
-    ContactList.destroy_contact(ARGV[1])
+  end #end-def
+  if  ARGV.empty?
+    puts "Here is a list of available commands: "
+    puts "new    - Create a new contact "
+    puts "list   - List all contacts "
+    puts "show   - Show a contact "
+    puts "search - Search contacts "
+
+  else
+    case ARGV[0]
+    when 'list'
+      ContactList.contact_index
+    when 'new'
+      ContactList.new_contact
+    when 'show'
+      ContactList.show_contact(ARGV[1])
+    when 'search'
+      ContactList.search_contact(ARGV[1])
+    when 'update'
+      ContactList.update_contact(ARGV[1])
+    when 'destroy'
+      ContactList.destroy_contact(ARGV[1])
+    end
   end
-end
 end
